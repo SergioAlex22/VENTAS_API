28fad2bf5b4bf9bceb4a03c8a3a2cd3e284836c4
Sergio <sergivalles113@gmail.com>
2024-05-14 15:57:04 -0500

    servicio creando de person_api

--- person_api/index.js
+++ person_api/index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express")
+const uri = 'mongodb+srv://admin:marcelo17@cluster1.sxvp7ge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require('mongoose');
+mongoose.connect(uri);
+const app = express()
+app.use( express.json() )
+const port = 8080
+const { peopleModel } = require('./models');
+app.get('/', (req, res) => { res.send("I am alive People"); })
+
+app.get('/people', async(req, res)=>{
+  const people = await peopleModel.find({});
+  res.json( people );
+});
+app.get('/people/:dni', async(req, res)=>{
+  const person = await peopleModel.find({dni:req.params.dni});
+  res.json( person );
+});
+app.post('/people', async(req, res)=>{
+  try {
+    const dni = req.body.dni;
+    const name = req.body.name;
+    const lastname = req.body.lastname;
+    const address = req.body.address;
+
+    const person = new peopleModel({ dni,name,lastname, address});
+
+    const data = await person.save();
+    return res.status(201).json(data);
+  } catch (error) {
+    console.log('Error', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
